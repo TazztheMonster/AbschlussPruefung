988f097c8451f9e9e4b2f891559c526aeed61902
Felix Pape <Felix@Pape.rocks>
2021-01-18 15:02:54 +0100

    Initial commit

---
+++ src/test/java/org/vw/f73/abschlusspruefung/AbschlusspruefungApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vw.f73.abschlusspruefung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbschlusspruefungApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
