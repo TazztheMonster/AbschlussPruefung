8729322c99ce89a39a3126483064b62f079c8335
Felix Pape <Felix@Pape.rocks>
2021-01-18 17:13:34 +0100

    Some git testing.

--- backend/src/main/java/org/vw/f73/abschlusspruefung/AbschlusspruefungApplication.java
+++ backend/src/main/java/org/vw/f73/abschlusspruefung/AbschlusspruefungApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AbschlusspruefungApplication {
 
     public static void main(String[] args) {
+		
         SpringApplication.run(AbschlusspruefungApplication.class, args);
     }
 
