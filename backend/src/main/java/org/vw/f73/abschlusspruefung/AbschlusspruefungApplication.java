acdb6337e292bb2fb09a4f6a2802fedf3e31713b
Felix Pape <Felix@Pape.rocks>
2021-01-18 17:02:23 +0100

    Moved backend into it's own directory.

---
+++ backend/src/main/java/org/vw/f73/abschlusspruefung/AbschlusspruefungApplication.java
@@ -0,0 +1,13 @@
+package org.vw.f73.abschlusspruefung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbschlusspruefungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbschlusspruefungApplication.class, args);
+    }
+
+}
