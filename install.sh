277019760b70184400af68626f4126746c090dbc
Felix Pape <Felix@Pape.rocks>
2021-01-22 15:22:37 +0100

    addet loadbalancer.yml and addet helm installation to install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
+helm repo add bitnami https://charts.bitnami.com/bitnami
+helm repo update
 helm install mongodb bitnami/mongodb
 mongodb-password=$(kubectl get secret --namespace default mongodb -o jsonpath="{.data.mongodb-root-password}")
